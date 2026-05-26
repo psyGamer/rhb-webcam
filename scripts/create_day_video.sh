7d0d4d4347855c7f0e44fb0fd67a74cec477dd7d
psyGamer <psygamer21@gmail.com>
2026-05-26 17:26:37 +0200

    Add NVIDIA hardware support to daily archive creation

--- scripts/create_day_video.sh
+++ scripts/create_day_video.sh
@@ -34,22 +34,45 @@ ls ${WEBCAM_ILANZ_SNIPPET:?}/$1 | while read -r file ; do
 done
 
 # Create mega-video for day
-if [ -f ${ARCHIVE_FILE_MIRALAGO:?} ]; then
-    ffmpeg -fflags +genpts -hwaccel qsv -hwaccel_output_format qsv -extra_hw_frames 16 \
-           -r 60 -f concat -safe 0 -i ${ARCHIVE_FILE_MIRALAGO:?} \
-           -movflags +faststart -vf "vpp_qsv=format=nv12" -c:v h264_qsv ${WEBCAM_MIRALAGO_DAILY:?}/$1.mp4
-fi
+if [ "$HARDWARE_ACCELERATION" == "intel" ]; then
+    if [ -f ${ARCHIVE_FILE_MIRALAGO:?} ]; then
+        ffmpeg -fflags +genpts -hwaccel qsv -hwaccel_output_format qsv -extra_hw_frames 16 \
+            -r 60 -f concat -safe 0 -i ${ARCHIVE_FILE_MIRALAGO:?} \
+            -movflags +faststart -vf "vpp_qsv=format=nv12" -c:v h264_qsv ${WEBCAM_MIRALAGO_DAILY:?}/$1.mp4
+    fi
 
-if [ -f ${ARCHIVE_FILE_ILANZ:?} ]; then
-    ffmpeg -fflags +genpts -hwaccel qsv -hwaccel_output_format qsv -extra_hw_frames 16 \
-           -r 60 -f concat -safe 0 -i ${ARCHIVE_FILE_ILANZ:?} \
-           -movflags +faststart -vf "vpp_qsv=format=nv12" -c:v h264_qsv ${WEBCAM_ILANZ_DAILY:?}/$1.mp4
-fi
+    if [ -f ${ARCHIVE_FILE_ILANZ:?} ]; then
+        ffmpeg -fflags +genpts -hwaccel qsv -hwaccel_output_format qsv -extra_hw_frames 16 \
+            -r 60 -f concat -safe 0 -i ${ARCHIVE_FILE_ILANZ:?} \
+            -movflags +faststart -vf "vpp_qsv=format=nv12" -c:v h264_qsv ${WEBCAM_ILANZ_DAILY:?}/$1.mp4
+    fi
 
-if [ -f ${ARCHIVE_FILE_FILISUR:?} ]; then
-    ffmpeg -fflags +genpts -hwaccel vaapi -hwaccel_output_format vaapi \
-           -f concat -safe 0 -i ${ARCHIVE_FILE_FILISUR:?} \
-           -movflags +faststart -vf 'hwmap=derive_device=qsv,format=qsv' -c:v h264_qsv -global_quality 40 -look_ahead 1 -preset veryfast -scenario videosurveillance ${WEBCAM_FILISUR_DAILY:?}/$1.mp4
+    if [ -f ${ARCHIVE_FILE_FILISUR:?} ]; then
+        ffmpeg -fflags +genpts -hwaccel vaapi -hwaccel_output_format vaapi \
+            -f concat -safe 0 -i ${ARCHIVE_FILE_FILISUR:?} \
+            -movflags +faststart -vf 'hwmap=derive_device=qsv,format=qsv' -c:v h264_qsv -global_quality 40 -look_ahead 1 -preset veryfast -scenario videosurveillance ${WEBCAM_FILISUR_DAILY:?}/$1.mp4
+    fi
+elif [ "$HARDWARE_ACCELERATION" == "nvidia" ]; then
+    if [ -f ${ARCHIVE_FILE_MIRALAGO:?} ]; then
+        ffmpeg -fflags +genpts -hwaccel cuda -hwaccel_output_format cuda -extra_hw_frames 16 \
+            -r 60 -f concat -safe 0 -i ${ARCHIVE_FILE_MIRALAGO:?} \
+            -movflags +faststart -vf "scale_cuda=format=nv12" -c:v h264_nvenc ${WEBCAM_MIRALAGO_DAILY:?}/$1.mp4
+    fi
+
+    if [ -f ${ARCHIVE_FILE_ILANZ:?} ]; then
+        ffmpeg -fflags +genpts -hwaccel cuda -hwaccel_output_format cuda -extra_hw_frames 16 \
+            -r 60 -f concat -safe 0 -i ${ARCHIVE_FILE_ILANZ:?} \
+            -movflags +faststart -vf "scale_cuda=format=nv12" -c:v h264_nvenc ${WEBCAM_ILANZ_DAILY:?}/$1.mp4
+    fi
+
+    if [ -f ${ARCHIVE_FILE_FILISUR:?} ]; then
+        ffmpeg -fflags +genpts -hwaccel cuda -hwaccel_output_format cuda \
+            -f concat -safe 0 -i ${ARCHIVE_FILE_FILISUR:?} \
+            -movflags +faststart -vf 'scale_cuda=format=nv12' -c:v h264_nvenc -cq 30 -preset p1 -tune hq ${WEBCAM_FILISUR_DAILY:?}/$1.mp4
+    fi
+else
+    echo "Unsupported hardware acceleration: $HARDWARE_ACCELERATION"
+    exit 1
 fi
 
 rm -rf ${WEBCAM_FILISUR_SNIPPET:?}/$1
